8ca3e6f3dd79f79cdc00d36c9fe3902837011353
Juan Martin Raimondo <juanmarraimondo@gmail.com>
2026-06-12 11:52:11 -0300

    Se agrega el framework bulma para agregarlo al proyecto final

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
-
   return (
-    <>
-      <h1>titulo
-
-      </h1>
-    </>
-  )
+    <div className="section">
+      <div className="container">
+        <h1 className="title">Trabajo Final Desarrollo Front-End</h1>
+        <p className="subtitle">Proyecto Almacén - Montero Santiago y Raimondo Juan Martin</p>
+        
+        
+        
+      </div>
+    </div>
+  );
 }
 
 export default App
